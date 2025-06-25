b432f88d4f59f2cdd8d88809db9b04f456531b17
DEN21R <denr@ukr.net>
2025-06-25 09:25:26 +0300

    update imageComponent

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -1,7 +1,6 @@
 function ImageComponent() {
   return (
     <div>
-      hi!
       <img
         src="https://th.bing.com/th/id/OIP.Bo1W5kVq1khutFTGklTQqAAAAA?w=185&h=147&c=7&r=0&o=7&pid=1.7&rm=3"
         alt="non"
